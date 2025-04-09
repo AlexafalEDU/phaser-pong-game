477eeade0256a378be2bf4fb21b77d5c38fb7bf9
AlexafalEDU <filbert_alexa_leander@students.edu.sg>
2025-04-09 08:51:42 +0000

    adding ball and paddle to the game

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -6,15 +6,31 @@ const HEIGHT = 768;
 export class Game extends Scene {
     constructor() {
         super('Game');
+
+        // create variable
+        this.ball = null;
+        this.leftPaddle = null;
+        this.rightPaddle = null;
     }
 
     preload() {
+        // load assets like images
+        this.load.image('background', 'assets/background.png');
+        this.load.image('ball', 'assets/ball.png');
+        this.load.image('paddle', 'assets/paddle.png');
     }
 
     create() {
+        // initialize game
+        // this.add.image(x, y, key);
+        this.add.image(WIDTH/2, HEIGHT/2, 'background').setScale(0.8, 0.8);
+        this.add.image(WIDTH/2, HEIGHT/2, 'ball').setScale(0.05, 0.05);
+        this.leftPaddle = this.add.image(50, 384, "paddle");
+        this.rightPaddle = this.add.image(974, 384, "paddle");
     }
 
     update() {
+        // game logic
     }
 
 }
